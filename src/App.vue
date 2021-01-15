fd4d4157dda46516381d7c5b5e0b647199cbc28a
Tequilian <cherryian@outlook.de>
2021-01-15 01:31:54 +0100

    Build Vue PWA

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%">
+    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+@import "assets/theme-builder.css";
+</style>
