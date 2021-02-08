96f7f4576db3af81617ffc153204ea575d472466
Kilian Domaratius <github@tequilian.de>
2021-02-08 18:24:45 +0100

    Add vcf file uploader

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="contacts">
     <h1>Contacts</h1>
+    <template>
+      <v-file-input
+        accept=".vcf,.txt"
+        label="Import .vcf-file"
+        @change="importVcf($event)"
+        multiple
+      ></v-file-input>
+    </template>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    importVcf: function(files) {
+      // Browser is not compatible
+      if (!window.FileReader) return;
+
+      // discard if no files given
+      if (files.length <= 0) return;
+
+      // get files content
+      files.forEach((file) => {
+        const reader = new FileReader();
+        reader.addEventListener("load", (event) => {
+          console.log(event.target.result);
+        });
+        reader.readAsText(file);
+      });
+
+      // convert to json
+
+    },
+  },
+};
+</script>
