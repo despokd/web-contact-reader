e600a4d17868d9c7cc1108eddf22bc9bee796f7c
Kilian Domaratius <github@tequilian.de>
2021-02-08 21:41:07 +0100

    Add erroir info

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -9,27 +9,61 @@
         multiple
       ></v-file-input>
     </template>
+
+    <v-snackbar v-model="snackbar.open" :timeout="snackbar.timeout">
+      {{ snackbar.text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          text
+          color="primary"
+          v-bind="attrs"
+          @click="snackbar.open = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      contacts: [],
+      snackbar: {
+        text: "Error",
+        timeout: "3000",
+        open: false,
+      },
+    };
+  },
   methods: {
     importVcf: function(event) {
       // Browser is not compatible
-      if (!window.FileReader) return;
-
-      console.log(event);
+      if (!window.FileReader) {
+        this.snackbar.text = "Browser is not compatible";
+        this.snackbar.open = true;
+        return;
+      }
 
       // discard if no files given
-      if (event.length <= 0) return;
+      if (event.length <= 0) {
+        this.snackbar.text = "No files given";
+        this.snackbar.open = true;
+        return;
+      }
 
       let fr = new FileReader();
       fr.onload = function() {
         // parse file to vCard
         let vCard = require("vcf");
         let cards = vCard.parse(fr.result);
-        console.log(cards);
+
+        // add to data list
+        cards.forEach((card) => {
+          this.data.contacts.push(card.toJSON()[1]);
+        });
       };
       fr.readAsText(event[0]);
     },
