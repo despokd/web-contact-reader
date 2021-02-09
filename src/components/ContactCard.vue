d986aa8de8075f5d62ba7f09d1621cbe7a63201a
Kilian Domaratius <github@tequilian.de>
2021-02-09 22:03:40 +0100

    Create edit dialog

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -5,38 +5,55 @@
         <pre>{{ contact }}</pre>
         <v-divider></v-divider>
         <v-list>
-          <v-list-item two-line>
+          <v-list-item two-line class="px-0">
             <v-list-item-avatar :color="randomColor">
-              <img v-if="contact.img.hasImg" :src="contact.img.src" :alt="contact.name.full" />
+              <img
+                v-if="contact.img.hasImg"
+                :src="contact.img.src"
+                :alt="contact.name.full"
+              />
               <span v-else class="headline">{{ contact.name.short }}</span>
             </v-list-item-avatar>
 
             <v-list-item-content>
               <v-list-item-title>{{ contact.name.full }}</v-list-item-title>
               <v-list-item-subtitle>
-                <span
-                  v-for="(item, index) in contact.org"
-                  :key="index"
-                  >{{ item }}</span
-                >
+                <span v-for="(item, index) in contact.org" :key="index">{{
+                  item
+                }}</span>
               </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-list>
       </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" text @click="dialog = true">
+          <v-icon left>
+            mdi-pencil
+          </v-icon>
+          Edit
+        </v-btn>
+      </v-card-actions>
     </v-card>
+    <ContactEdit :open="dialog" :contact="contact"/>
+    
   </div>
 </template>
 
 <script>
+import ContactEdit from '@/components/ContactEdit.vue';
+
 export default {
   name: "contact-card",
   props: ["contact"],
+  components: {
+    ContactEdit,
+  },
   data: () => {
     return {
+      dialog: false,
       randomColor: "#" + ((Math.random() * 0xffffff) << 0).toString(16),
     };
   },
 };
-
 </script>
